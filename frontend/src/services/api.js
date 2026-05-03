620082c3ee3acd72943881d00b140fc65e03a607
RAHUL THORIAPATI <rahulthoriapati@RAHULs-MacBook-Air-137.local>
2026-05-03 17:27:42 +0530

    fixed API variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API = "http://localhost:5001/api";
+const BASE_URL = "https://developer-productivity-mvp-des3.onrender.com";
 
 export const getMetrics = async (developerId) => {
-  const res = await axios.get(`${API}/metrics/${developerId}`);
+  const res = await axios.get(`${BASE_URL}/metrics/${developerId}`);
   return res.data;
 };
