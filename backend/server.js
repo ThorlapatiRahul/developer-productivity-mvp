fa2b93cd7329f0559e79aa80c104886440df2b54
RAHUL THORIAPATI <rahulthoriapati@RAHULs-MacBook-Air-137.local>
2026-05-03 17:21:07 +0530

    fix render port issue

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-import app from "./app.js";
-
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
