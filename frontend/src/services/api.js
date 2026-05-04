ccecd874f3e477751b1b5fe4b38e503c78d40dbe
RAHUL THORIAPATI <rahulthoriapati@RAHULs-MacBook-Air-137.local>
2026-05-04 10:47:38 +0530

    fix production api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://developer-productivity-mvp-des3.onrender.com";
 
 export const getMetrics = async (username) => {
+  console.log("CALLING:", `${BASE_URL}/api/metrics/${username}`); // debug
   const res = await axios.get(`${BASE_URL}/api/metrics/${username}`);
   return res.data;
 };
